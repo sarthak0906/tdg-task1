e871020aa71dad1745c1a1d10ea3b99974920d7e
sarthak khandelwal <sarthak0906@gmail.com>
2019-05-03 18:21:16 +0530

    update

--- front/student.js
+++ front/student.js
@@ -1,57 +1,18 @@
-var email = "";
 $(document).ready(()=>{
-    email = document.location.href;
-    email = email.split('?')[1];
-    email = email.split('=')[1];
-    console.log(email);
-    var current = 1;
-    disable_edit();
-    $("#submit_name").on('click',(event)=>{
+    var mail = $('#email');
+    var phone = $('#phone');
+    var name = $('#name');
+    $('#submit').on('click',(event)=>{
         event.preventDefault();
-        if(current){
-            enable_edit();
-            current = 0;
+        console.log("asdasd");
+        res = {"type":"student"};
+        if(res.type == "student"){
+            window.location.replace("./student.html?mail="+mail.val());
         }else{
-            $.getJSON("https://tdg-ap.herokuapp.com/student?name"+$('#name').val()+"&email="+$('#email').val()+"&phone="+$('#phone').val(),(res)=>{
-                alert("Changes Applied");
-                disable_edit();
-            });
-            current = 1;
+            window.location.replace("./admin.html?mail="+mail.val());
         }
+        $.getJSON("https://tdg-ap.herokuapp.com/login?email="+mail.val()+"&name="+name.val()+"&phone="+phone.val(),(res)=>{
+            console.log(res);
+        });
     });
 });
-
-function disable_edit(){
-    html = "";
-    $.getJSON("api/get?email="+email,(data)=>{
-        $('#inner_entry_box').html("");
-        disable_name_form("name",data.name,"Name");
-        disable_name_form("email",data.email,"E-Mail");
-        disable_name_form("phone",data.phone,"Phone Number");
-    });
-}
-function enable_edit(){
-    html = "";
-    $.getJSON("api/get?email="+email,(data)=>{
-        $('#inner_entry_box').html("");
-        enable_name_form("name",data.name,"Name");
-        enable_name_form("email",data.email,"E-Mail");
-        enable_name_form("phone",data.phone,"Phone Number");
-    });
-}
-
-
-function disable_name_form(id,value,message){
-    var entry = "";
-    entry += '<fieldset disabled><div class="input-group mb-3"><div class="input-group-prepend"><span class="input-group-text" id="basic-addon1">'+message+'</span></div><input id = "'+id+'" type="text" class="form-control" placeholder="Team Name" aria-label="Username" aria-describedby="basic-addon1"></div></fieldset>';
-    $('#inner_entry_box').append(entry);
-    $('#'+id).val(value);
-    $('#submit_name').text("Edit");
-}
-function enable_name_form(id,value,message){
-    var entry = "";
-    entry += '<div class="input-group mb-3"><div class="input-group-prepend"><span class="input-group-text" id="basic-addon1">'+message+'</span></div><input id = "'+id+'" type="text" class="form-control" placeholder="Team Name" aria-label="Username" aria-describedby="basic-addon1"></div>';
-    $('#inner_entry_box').append(entry);
-    $('#'+id).val(value);
-    $('#submit_name').text("Submit");
-}
