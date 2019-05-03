a1936e8d2c3301f6bca5cf43bb58d63439d5cc97
sarthak khandelwal <sarthak0906@gmail.com>
2019-05-03 18:28:03 +0530

    Update student.js

--- front/student.js
+++ front/student.js
@@ -4,13 +4,7 @@ $(document).ready(()=>{
     var name = $('#name');
     $('#submit').on('click',(event)=>{
         event.preventDefault();
-        console.log("asdasd");
-        res = {"type":"student"};
-        if(res.type == "student"){
-            window.location.replace("./student.html?mail="+mail.val());
-        }else{
-            window.location.replace("./admin.html?mail="+mail.val());
-        }
+        // console.log("asdasd");
         $.getJSON("https://tdg-ap.herokuapp.com/student?email="+mail.val()+"&name="+name.val()+"&phone="+phone.val(),(res)=>{
             console.log(res);
         });
